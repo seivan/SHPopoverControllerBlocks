76b1908c13c80e4bd4271fd506fb82a64e4252ed
Seivan Heidari <seivan@kth.se>
2013-07-15 02:55:38 +0200

    DOne, time for docs and spec.

--- Example/Example/SHViewController.m
+++ Example/Example/SHViewController.m
@@ -12,8 +12,13 @@
 #import "SHSecondViewController.h"
 #import "SHPopoverControllerBlocks.h"
 #import "SHBarButtonItemBlocks.h"
+#import "SHControlBlocks.h"
+
 @interface SHViewController ()
+<UIPopoverControllerDelegate>
+
 @property(nonatomic,strong) UIPopoverController * popController;
+
 @end
 
 @implementation SHViewController
@@ -27,6 +32,7 @@
   [super viewDidAppear:animated];
   SHSecondViewController * secondVc = [[UIStoryboard storyboardWithName:@"MainStoryboard" bundle:nil] instantiateViewControllerWithIdentifier:@"second"];
   self.popController = [[UIPopoverController alloc] initWithContentViewController:secondVc];
+  self.popController.popoverContentSize = CGSizeMake(300, 300);
 
   UIBarButtonItem * barButtonItem = self.navigationItem.rightBarButtonItem;
   [barButtonItem SH_addBlock:^(UIBarButtonItem *sender) {
@@ -34,11 +40,39 @@
       [_popController dismissPopoverAnimated:YES];
     else
       [_popController presentPopoverFromBarButtonItem:barButtonItem permittedArrowDirections:UIPopoverArrowDirectionUp animated:YES];
+    
+  }];
+
+  __block BOOL testShouldDismiss = NO;
+  __block BOOL testDidDismiss    = NO;
+
+  
+  dispatch_semaphore_t semaphoreShouldDismiss = dispatch_semaphore_create(0);
+  dispatch_semaphore_t semaphoreDidDismiss    = dispatch_semaphore_create(0);
+  
+  [self.popController SH_setShouldDismissPopoverBlock:^BOOL(UIPopoverController *thePopoverController) {
+    testShouldDismiss = YES;
+    dispatch_semaphore_signal(semaphoreShouldDismiss);
+    return YES;
   }];
   
+  [self.popController SH_setDidDismissPopoverBlock:^(UIPopoverController *thePopoverController) {
+    testDidDismiss = YES;
+    dispatch_semaphore_signal(semaphoreDidDismiss);
+  }];
+  
+  dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+    dispatch_semaphore_wait(semaphoreShouldDismiss, DISPATCH_TIME_FOREVER);
+    SHBlockAssert(testShouldDismiss, @"testShouldDismiss should be YES");
+    
+    dispatch_semaphore_wait(semaphoreShouldDismiss, DISPATCH_TIME_FOREVER);
+    SHBlockAssert(testDidDismiss, @"testDidDismissw should be YES");
+  });
   
   
 }
+
+
 
 
 @end
--- Example/Pods/Pods-environment.h
+++ Example/Pods/Pods-environment.h
@@ -12,6 +12,12 @@
 #define COCOAPODS_VERSION_MINOR_SHBarButtonItemBlocks 0
 #define COCOAPODS_VERSION_PATCH_SHBarButtonItemBlocks 0
 
+// SHControlBlocks
+#define COCOAPODS_POD_AVAILABLE_SHControlBlocks
+#define COCOAPODS_VERSION_MAJOR_SHControlBlocks 1
+#define COCOAPODS_VERSION_MINOR_SHControlBlocks 0
+#define COCOAPODS_VERSION_PATCH_SHControlBlocks 1
+
 // SHPopoverControllerBlocks
 #define COCOAPODS_POD_AVAILABLE_SHPopoverControllerBlocks
 #define COCOAPODS_VERSION_MAJOR_SHPopoverControllerBlocks 1
