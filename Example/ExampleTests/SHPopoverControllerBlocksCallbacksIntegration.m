00c863ae6f021b428b4bdafbf43d083e9067bb60
Seivan Heidari <seivan@kth.se>
2013-08-03 10:01:59 +0200

    Update .travis.yml with xctool -workspace Example.xcworkspace -scheme Example build test -simulator ipad -sdk iphonesimulator

--- Example/ExampleTests/SHPopoverControllerBlocksCallbacksIntegration.m
+++ Example/ExampleTests/SHPopoverControllerBlocksCallbacksIntegration.m
@@ -1,154 +1,152 @@
-////
-////  SHActionSheetBlocksCallbacksScenarion.m
-////  Example
-////
-////  Created by Seivan Heidari on 7/31/13.
-////  Copyright (c) 2013 Seivan Heidari. All rights reserved.
-////
 //
-//#import "KIF.h"
-//#import "SHPopoverControllerBlocksSuper.h"
+//  SHActionSheetBlocksCallbacksScenarion.m
+//  Example
 //
+//  Created by Seivan Heidari on 7/31/13.
+//  Copyright (c) 2013 Seivan Heidari. All rights reserved.
 //
-//@interface SHActionSheetBlocksCallbacksIntegration : KIFTestCase
-//@property(nonatomic,strong) UIViewController    * vc;
-//@property(nonatomic,strong) UIActionSheet       * sheet;
-//
-//
-//@end
-//
-//@implementation SHActionSheetBlocksCallbacksIntegration
-//-(void)beforeEach; {
-//  [super beforeEach];
-//  
-//  
-//  self.sheet = [UIActionSheet SH_actionSheetWithTitle:@"Title"];
-//  self.vc    = UIViewController.new;
-//
-//  
-//  [UIApplication sharedApplication].keyWindow.rootViewController = self.vc;
-//  self.sheet = [UIActionSheet SH_actionSheetWithTitle:@"Title"];
-//
-//  
-//}
-//
-//
-//-(void)testSingleButton; {
-//  NSString * buttonTitle   = @"Button1";
-//  __block BOOL didPassTest = NO;
-//  [self.sheet SH_addButtonWithTitle:buttonTitle withBlock:^(NSInteger theButtonIndex) {
-//    STAssertEquals(theButtonIndex, 0, nil);
-//    didPassTest = YES;
-//  }];
-//  [self.sheet showInView:self.vc.view];
-//  [tester tapViewWithAccessibilityLabel:buttonTitle];
-//  
-//  STAssertTrue(didPassTest, nil);
-//  
-//}
-//
-//-(void)testDestructiveButton; {
-//  NSString * buttonTitle   = @"Delete";
-//  __block BOOL didPassTest = NO;
-//  [self.sheet SH_addButtonDestructiveWithTitle:buttonTitle withBlock:^(NSInteger theButtonIndex) {
-//    STAssertEquals(theButtonIndex, 0, nil);
-//    STAssertEquals(theButtonIndex, self.sheet.destructiveButtonIndex, nil);
-//    didPassTest = YES;
-//  }];
-//  [self.sheet showInView:self.vc.view];
-//  [tester tapViewWithAccessibilityLabel:buttonTitle];
-//  
-//  STAssertTrue(didPassTest, nil);
-//  
-//}
-//
-//-(void)testCancelButton; {
-//  NSString * buttonTitle   = @"Cancel";
-//  __block BOOL didPassTest = NO;
-//  [self.sheet SH_addButtonCancelWithTitle:buttonTitle withBlock:^(NSInteger theButtonIndex) {
-//    STAssertEquals(theButtonIndex, 0, nil);
-//    STAssertEquals(theButtonIndex, self.sheet.cancelButtonIndex, nil);
-//    didPassTest = YES;
-//  }];
-//  [self.sheet showInView:self.vc.view];
-//  [tester tapViewWithAccessibilityLabel:buttonTitle];
-//  
-//  STAssertTrue(didPassTest, nil);
-//}
-//
-//-(void)testSingleBlock; {
-//  
-//  NSArray * buttons = @[@"Button", @"Cancel", @"Delete"];
-//  
-//  [buttons enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL *stop) {
-//    [self withButtonList:buttons.mutableCopy withButton:obj];
-//  }];
-//}
-//
-//-(void)withButtonList:(NSMutableArray *)theButtonList withButton:(NSString *)theButtonTitle ; {
-//  __block BOOL didPassTest = NO;
-//  self.sheet = [UIActionSheet SH_actionSheetWithTitle:@"Title"
-//                                         buttonTitles:@[[theButtonList SH_popFirstObject]]
-//                                          cancelTitle:[theButtonList SH_popFirstObject]
-//                                     destructiveTitle:[theButtonList SH_popFirstObject]
-//                                            withBlock:^(NSInteger theButtonIndex) {
-//                                              didPassTest = YES;
-//                                              
-//                                              
-//                                            }];
-//  [self.sheet showInView:self.vc.view];
-//  [tester tapViewWithAccessibilityLabel:theButtonTitle];
-//  STAssertTrue(didPassTest, nil);
-//
-//}
-//
-//-(void)testSheetAppearanceLifeCycleBlocks; {
-//  
-//    NSString * buttonTitle   = @"Delete";
-//  
-//  __block BOOL willShowBlock    = NO;
-//  __block BOOL didShowBlock     = NO;
-//  __block BOOL willDismissBlock = NO;
-//  __block BOOL didDismissBlock  = NO;
-//  
-////  [self SH_performAsyncTestsWithinBlock:^(BOOL *didFinish) {
-//    
-//    [self.sheet SH_addButtonDestructiveWithTitle:buttonTitle withBlock:^(NSInteger theButtonIndex) {}];
-//    
-//    [self.sheet SH_setWillShowBlock:^(UIActionSheet *theActionSheet) {
-//      willShowBlock = YES;
-//    }];
-//    
-//    [self.sheet SH_setDidShowBlock:^(UIActionSheet *theActionSheet) {
-//      didShowBlock = YES;
-//    }];
-//    
-//    [self.sheet SH_setWillDismissBlock:^(UIActionSheet *theActionSheet, NSInteger theButtonIndex) {
-//      willDismissBlock = YES;
-//      STAssertEqualObjects(self.sheet, theActionSheet, nil);
-//      STAssertEquals(theButtonIndex, theActionSheet.destructiveButtonIndex, nil);
-//    }];
-//    
-//    [self.sheet SH_setDidDismissBlock:^(UIActionSheet *theActionSheet, NSInteger theButtonIndex) {
-//      didDismissBlock = YES;
-//      STAssertEqualObjects(self.sheet, theActionSheet, nil);
-//      STAssertEquals(theButtonIndex, theActionSheet.destructiveButtonIndex, nil);
-//    }];
-//  
-//  [self.sheet showInView:self.vc.view];
-//  [tester tapViewWithAccessibilityLabel:buttonTitle];
-////  } withTimeout:5];
-//  
-//  
-//  STAssertTrue(willShowBlock, nil);
-//  STAssertTrue(didShowBlock, nil);
-//  STAssertTrue(willDismissBlock, nil);
-//  STAssertTrue(didDismissBlock, nil);
-//  
-//  
-//}
-//
-//
-//
-//
-//@end
+
+#import "KIF.h"
+#import "SHPopoverControllerBlocksSuper.h"
+
+
+@interface SHPopoverControllerBlocksCallbacksIntegration : KIFTestCase
+@property(nonatomic,strong) UIViewController    * vc;
+@property(nonatomic,strong) UIPopoverController * popoverController;
+
+
+@end
+
+@implementation SHPopoverControllerBlocksCallbacksIntegration
+-(void)beforeEach; {
+  [super beforeEach];
+  self.vc = UIViewController.new;
+  
+  UIViewController * popVc = UIViewController.new;
+  popVc.view.frame = CGRectMake(0, 0, 200, 200);
+  self.popoverController     = [[UIPopoverController alloc]
+                                initWithContentViewController:popVc];
+  [UIApplication sharedApplication].keyWindow.rootViewController = self.vc;
+  [self.popoverController presentPopoverFromRect:CGRectMake(0, 0, 200, 200) inView:self.vc.view permittedArrowDirections:UIPopoverArrowDirectionDown animated:NO];
+  [UIApplication sharedApplication].keyWindow.rootViewController = self.vc;
+
+  
+}
+
+
+-(void)testSH_blockShouldDismissPopoverBlockAsYes; {
+
+  __block BOOL testShouldDismiss = NO;
+  [self SH_performAsyncTestsWithinBlock:^(BOOL *didFinish) {
+    [self.popoverController SH_setShouldDismissPopoverBlock:^BOOL(UIPopoverController *thePopoverController) {
+      testShouldDismiss = YES;
+      return YES;
+    }];
+    [tester tapScreenAtPoint:CGPointMake(250, 250)];
+    
+    double delayInSeconds = 0.5;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+      *didFinish = YES;
+    });
+  } withTimeout:5];
+  STAssertTrue(testShouldDismiss, nil);
+  STAssertFalse(self.popoverController.isPopoverVisible, nil);
+}
+
+-(void)testSH_blockShouldDismissPopoverBlockAsNo; {
+  __block BOOL testShouldDismiss = NO;
+  
+  [self SH_performAsyncTestsWithinBlock:^(BOOL *didFinish) {
+    [self.popoverController SH_setShouldDismissPopoverBlock:^BOOL(UIPopoverController *thePopoverController) {
+      testShouldDismiss = YES;
+      return NO;
+    }];
+    [tester tapScreenAtPoint:CGPointMake(250, 250)];
+    
+    double delayInSeconds = 0.5;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+      *didFinish = YES;
+    });
+  } withTimeout:5];
+  STAssertTrue(testShouldDismiss, nil);
+  STAssertTrue(self.popoverController.isPopoverVisible, nil);
+}
+
+-(void)testSH_blockDidDismissPopoverBlock; {
+  __block BOOL testDidDismiss = NO;
+  
+  [self SH_performAsyncTestsWithinBlock:^(BOOL *didFinish) {
+    [self.popoverController SH_setDidDismissPopoverBlock:^(UIPopoverController *thePopoverController) {
+      testDidDismiss = YES;
+    }];
+    [tester tapScreenAtPoint:CGPointMake(250, 250)];
+    
+    double delayInSeconds = 0.5;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+      *didFinish = YES;
+    });
+  } withTimeout:5];
+  STAssertTrue(testDidDismiss, nil);
+  STAssertFalse(self.popoverController.isPopoverVisible, nil);
+}
+
+-(void)testSH_blockBothCallBacksShouldNot; {
+  
+  __block BOOL testShouldDismiss = NO;
+  __block BOOL testDidDismiss = NO;
+  [self SH_performAsyncTestsWithinBlock:^(BOOL *didFinish) {
+    [self.popoverController SH_setShouldDismissPopoverBlock:^BOOL(UIPopoverController *thePopoverController) {
+      testShouldDismiss = YES;
+      return NO;
+    }];
+    
+    [self.popoverController SH_setDidDismissPopoverBlock:^(UIPopoverController *thePopoverController) {
+      testDidDismiss = YES;
+    }];
+    [tester tapScreenAtPoint:CGPointMake(250, 250)];
+    
+    double delayInSeconds = 0.5;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+      *didFinish = YES;
+    });
+  } withTimeout:5];
+  STAssertTrue(testShouldDismiss, nil);
+  STAssertFalse(testDidDismiss, nil);
+  STAssertTrue(self.popoverController.isPopoverVisible, nil);
+}
+
+
+-(void)testSH_blockBothCallBacksShould; {
+  
+  __block BOOL testShouldDismiss = NO;
+  __block BOOL testDidDismiss = NO;
+  [self SH_performAsyncTestsWithinBlock:^(BOOL *didFinish) {
+    [self.popoverController SH_setShouldDismissPopoverBlock:^BOOL(UIPopoverController *thePopoverController) {
+      testShouldDismiss = YES;
+      return YES;
+    }];
+    
+    [self.popoverController SH_setDidDismissPopoverBlock:^(UIPopoverController *thePopoverController) {
+      testDidDismiss = YES;
+    }];
+    [tester tapScreenAtPoint:CGPointMake(250, 250)];
+    
+    double delayInSeconds = 0.5;
+    dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
+    dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
+      *didFinish = YES;
+    });
+  } withTimeout:5];
+  STAssertTrue(testShouldDismiss, nil);
+  STAssertTrue(testDidDismiss, nil);
+  STAssertFalse(self.popoverController.isPopoverVisible, nil);
+}
+
+
+
+
+@end
